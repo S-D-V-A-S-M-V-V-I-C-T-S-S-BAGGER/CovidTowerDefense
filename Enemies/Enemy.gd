adbfeb02aac68da423b41825ed6346e6758ed3f7
Joost Loohuis <j.e.loohuis@gmail.com>
2021-05-07 21:46:05 +0200

    Made enemy path pickable

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -5,12 +5,14 @@ class_name Enemy
 # Declare member variables here. Examples:
 export(int) var speed = 300.0
 
+export(NodePath) var path
+
 var velocity := Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	get_node("Path").curve = get_node(path).curve
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
