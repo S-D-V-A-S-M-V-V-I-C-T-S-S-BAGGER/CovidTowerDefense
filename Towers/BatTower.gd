3d55718218c29aee5870d57ac44c12360f071598
Joost Loohuis <j.e.loohuis@gmail.com>
2021-05-07 23:52:43 +0200

    Added bat tower

---
+++ Towers/BatTower.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+export(int) var max_bat_count = 3
+export(float) var time_between_bats = 3.0
+
+
+export var bat_count := 1
+export var bat_timer := 0.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_bat_animation()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if bat_timer < time_between_bats:
+		# Increase the timer
+		bat_timer += delta
+	else:
+		if bat_count < max_bat_count:
+			# Add a bat
+			bat_count += 1
+			set_bat_animation()
+			
+			bat_timer = 0.0
+
+
+func set_bat_animation():
+	match bat_count:
+		0:
+			$Bat/AnimatedSprite.play("zero")
+		1:
+			$Bat/AnimatedSprite.play("one")
+		2:
+			$Bat/AnimatedSprite.play("two")
+		3:
+			$Bat/AnimatedSprite.play("three")
+		_:
+			$Bat/AnimatedSprite.play("many")
+
+
+func _on_Bat_area_entered(area: Area2D) -> void:
+	if bat_count > 0:
+		bat_count -= 1
+		set_bat_animation()
+		
+		var enemy = get_node(area.get_path()).get_parent().get_parent().get_parent()
+		var enemy_speed_modifer = enemy.get("speed_modifier")
+		enemy_speed_modifer *= 0.5
+		enemy.set("speed_modifier", enemy_speed_modifer)
