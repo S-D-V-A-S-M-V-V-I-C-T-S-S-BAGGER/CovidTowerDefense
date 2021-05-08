628612fce0aae1c467673f514f760b59d4c5f356
Joost Loohuis <j.e.loohuis@gmail.com>
2021-05-08 20:49:15 +0200

    Added healing range to healing enemy

--- Enemies/HealingEnemy.gd
+++ Enemies/HealingEnemy.gd
@@ -8,6 +8,7 @@ export(int) var heal_units = 4
 
 var last_cast = 0.0
 
+var in_range : Array = []
 
 func _process(delta: float) -> void:
 	._process(delta)
@@ -23,3 +24,14 @@ func _process(delta: float) -> void:
 
 func do_heal():
 	pass
+
+
+func _on_Healing_area_entered(area: Area2D) -> void:
+	var enemy : Enemy = area.owner
+	if !in_range.has(enemy):
+		in_range.append(enemy)
+
+
+func _on_Healing_area_exited(area: Area2D) -> void:
+	var enemy : Enemy = area.owner
+	in_range.erase(enemy)
