967a0f60c7567e21d7d09147f5d108b0361ac79f
Joost Loohuis <j.e.loohuis@gmail.com>
2021-05-09 18:46:07 +0200

    Remove particles after 10 seconds

--- Enemies/CoronaParticle.gd
+++ Enemies/CoronaParticle.gd
@@ -9,6 +9,7 @@ export (float) var duration = 10.0
 export (float) var dps = 1.0
 export (float) var infectiousness = 0.01
 
+export (float) var time_to_live = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,8 +17,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	time_to_live -= delta
+	
+	if time_to_live <= 0:
+		$Collider/CollisionShape.disabled = true
+		queue_free()
 
 
 func _on_Collider_area_entered(area: Area2D) -> void:
