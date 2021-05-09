31656a7838156276121f1fb14c66e646a2382d8a
Karel van Klink <karel@deob.us>
2021-05-09 14:28:22 +0200

    remove type hinten now 100 beter, add second wave to level 1

--- Enemies/AudioStreamPlayer.gd
+++ Enemies/AudioStreamPlayer.gd
@@ -9,7 +9,6 @@ const tracks = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14',
 func play_random_sound():
 	random.randomize()
 	var rand_nb = random.randi_range(0, tracks.size() - 1)
-	print(rand_nb)
 	var audiostream = load('res://Sounds/cough-files/cough' + tracks[rand_nb] + '.mp3')
 	
 	set_stream(audiostream)
