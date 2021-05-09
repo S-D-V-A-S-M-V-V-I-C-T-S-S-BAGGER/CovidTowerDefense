afe15be3ad197ee0f27b3f19f54ec6f6d0a5c897
BuildTools <unconfigured@null.spigotmc.org>
2021-05-09 17:31:15 +0200

    implemented level 3 and some waves in lvl 1/2

--- Menus/MainMenu/MainMenu.gd
+++ Menus/MainMenu/MainMenu.gd
@@ -1,6 +1,11 @@
 extends ColorRect
 
 export (PackedScene) var level1_scene
+export (PackedScene) var level2_scene
 
 func _on_Level1Button_pressed() -> void:
 	get_tree().change_scene_to(level1_scene)
+
+
+func _on_Level2Button_pressed() -> void:
+	get_tree().change_scene_to(level2_scene)
